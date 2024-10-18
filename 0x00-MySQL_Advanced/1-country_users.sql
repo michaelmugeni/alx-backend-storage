bd1b2cb56626b9d094a2e799ab58ff0d3300cd57
michaelmugeni <michaelmugeni98@gmail.com>
2024-10-18 08:50:12 +0000

    added updated 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- Creates a table with unique users.
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users(
-	id INT NOT NULL AUTO-INCREAMENT PRIMARY KEY,
-	email VARCHAR (255)NOT NULL UNIQUE,
-	name VARCHAR (255),
-	country char(2) NOT NULL defualt 'US' check (country IN('US', 'CO', 'TN'))
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
 );
