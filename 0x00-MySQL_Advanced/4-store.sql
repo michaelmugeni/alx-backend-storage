5b30f795affb9a9345e7da9a0db1a126cbac02b8
michaelmugeni <michaelmugeni98@gmail.com>
2024-10-18 11:20:00 +0000

    4-store.sql added

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,20 +1,5 @@
--- Initial setup: Create items and orders tables
-
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS orders;
-
-CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(255) NOT NULL,
-    quantity INT NOT NULL DEFAULT 10
-);
-
-CREATE TABLE IF NOT EXISTS orders (
-    item_name VARCHAR(255) NOT NULL,
-    number INT NOT NULL
-);
-
-INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
-
+-- Creates a trigger that decreases the quantity
+-- of an item after adding a new order.
 DROP TRIGGER IF EXISTS reduce_quantity;
 DELIMITER $$
 CREATE TRIGGER reduce_quantity
@@ -26,19 +11,3 @@ BEGIN
         WHERE name = NEW.item_name;
 END $$
 DELIMITER ;
-
--- Show and add orders
-
-SELECT * FROM items;
-SELECT * FROM orders;
-
--- Insert new orders
-INSERT INTO orders (item_name, number) VALUES ('apple', 1);
-INSERT INTO orders (item_name, number) VALUES ('apple', 3);
-INSERT INTO orders (item_name, number) VALUES ('pear', 2);
-
--- Verify changes
-SELECT "--";
-SELECT * FROM items;
-SELECT * FROM orders;
-
