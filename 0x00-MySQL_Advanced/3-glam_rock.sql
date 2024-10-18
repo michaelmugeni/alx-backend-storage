f9f4949da939f7be6859a56eaaf00e2af24d3fd3
michaelmugeni <michaelmugeni98@gmail.com>
2024-10-18 09:23:10 +0000

    task 3 complete

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
---Lista all bands with Glam rock as main style, ranked by longevity.
---SELECT band_name, (IFNULL(split,year(CURRENT DATE())) - FORMED) As lifespan
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-ORDER BY lifespan DESC;
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
