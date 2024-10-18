86006111baf4325fb311d709c5c436ea48093c3c
michaelmugeni <michaelmugeni98@gmail.com>
2024-10-18 09:42:03 +0000

    added 4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,15 +1,44 @@
--- SQL script to create a trigger that decreases the quantity of an item
--- after adding a new order.
+-- Initial setup: Create items and orders tables
 
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE IF NOT EXISTS items (
+    name VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL DEFAULT 10
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    item_name VARCHAR(255) NOT NULL,
+    number INT NOT NULL
+);
+
+INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
+
+DROP TRIGGER IF EXISTS reduce_quantity;
 DELIMITER $$
-
-CREATE TRIGGER decrease_quantity_after_order
+CREATE TRIGGER reduce_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE item_id = NEW.item_id;
-END$$
-
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
 DELIMITER ;
+
+-- Show and add orders
+
+SELECT * FROM items;
+SELECT * FROM orders;
+
+-- Insert new orders
+INSERT INTO orders (item_name, number) VALUES ('apple', 1);
+INSERT INTO orders (item_name, number) VALUES ('apple', 3);
+INSERT INTO orders (item_name, number) VALUES ('pear', 2);
+
+-- Verify changes
+SELECT "--";
+SELECT * FROM items;
+SELECT * FROM orders;
+
