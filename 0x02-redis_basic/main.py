73cd4a20ab943282f3633516e22502efeb193abe
michaelmugeni <michaelmugeni98@gmail.com>
2024-10-24 11:44:19 +0000

    main.py

---
+++ 0x02-redis_basic/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Main file to demonstrate the Cache class.
+"""
+
+import redis
+Cache = __import__('exercise').Cache
+
+cache = Cache()
+data = b"hello"
+key = cache.store(data)
+print(key)
+
+local_redis = redis.Redis()
+print(local_redis.get(key))
